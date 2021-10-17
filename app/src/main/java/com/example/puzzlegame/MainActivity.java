e997376c47a03f9bd34a6e6af57dee6f57f52ad0
Aression <57360540+Aression@users.noreply.github.com>
2021-10-17 18:19:06 +0800

    更新组件逻辑

--- app/src/main/java/com/example/puzzlegame/MainActivity.java
+++ app/src/main/java/com/example/puzzlegame/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
